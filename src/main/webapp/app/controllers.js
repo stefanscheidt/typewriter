b99b7403b255dbdf529ff1d7b9d1614227e055e5
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2012-08-08 15:09:04 +0200

    Generalized after review with @tigbro.

--- src/main/webapp/app/controllers.js
+++ src/main/webapp/app/controllers.js
@@ -1,10 +1,5 @@
 function TwController($scope) {
     $scope.currentChar = 'A';
-    $scope.processKeydown = function (event) {
-        var key = String.fromCharCode(event.which);
-        console.log('processKey: ' + key);
-        $scope.currentChar = key;
-    }
 }
 TwController.$inject = ['$scope'];
 
--- src/main/webapp/app/directives.js
+++ src/main/webapp/app/directives.js
@@ -1,13 +1,14 @@
 angular.module('tw.directives', [])
-    .directive('twDocumentKeydown', function () {
+    .directive('twDocumentKeydown', ['$parse', '$rootElement', function ($parse, $rootElement) {
         return {
             restrict:'A',
             link:function (scope, element, attrs) {
-                $(document).keydown(function (event) {
-                    scope[attrs['twDocumentKeydown']](event);
-                    scope.$digest();
+                var expr = $parse(attrs['twDocumentKeydown']);
+                $rootElement.keydown(function (event) {
+                    scope.$apply(function(scope) {
+                        expr(scope, {key:String.fromCharCode(event.which)});
+                    });
                 });
             }
         };
-    });
-
+    }]);
